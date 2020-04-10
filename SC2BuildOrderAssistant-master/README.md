bf7a6ddbdb23c4d281b91f38f37d9b1af2fb7296
Gage Anderson <gwanders380@gmail.com>
2020-04-10 17:00:04 -0400

    initial commit

---
+++ SC2BuildOrderAssistant-master/README.md
@@ -0,0 +1,2 @@
+# SC2BuildOrderAssistant
+A repository, forum, and web scraping tool to discuss and compile build orders for Starcraft 2.
---
+++ SC2BuildOrderAssistant_Venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\illum\Documents\SoftwareProjects\SC2BuildOrderAssistant\SC2BuildOrderAssistant_Venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
